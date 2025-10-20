5da3a3a081d1e74b5e8189234f562e05a443ceb5
anthonykravchishin <tonykravc53@gmail.com>
2025-10-20 16:25:17 -0700

    Added code to add entries into the division dropdown

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,15 @@
 import { Department } from "./departments.js";
+
+const departments = [
+	new Department("Fine Arts", "Christie Gilliland", "Liz Peterson", "Monica Bowen", "Paul Metevier"), 
+	new Department("Humanities", "Jamie Fitzgerald", "Liz Peterson", "Lisa Luengo", "Katie Cunnion "), 
+	new Department("Social Science", "Christie Gilliland", "Liz Peterson", "Joy Crawford", "Mark Thomason")
+];
+
+const list = document.getElementById('division');
+for (var i = 0; i < departments.length; i++) {
+	const entry = document.createElement('option');
+	entry.setAttribute("value", departments[i].getDivName().toLowerCase());
+	entry.textContent = departments[i].getDivName();
+	list.appendChild(entry);
+}

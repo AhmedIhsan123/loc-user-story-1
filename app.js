97c26ada7ab1dcc5158e5238951d9dd0ea45bb72
anthonykravchishin <tonykravc53@gmail.com>
2025-10-20 15:31:16 -0700

    Hosted on Digital Ocean

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 "Use strict";
+import express from 'express';
+
+const app = express();
+
+const PORT = 3003;
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.sendFile(`${import.meta.dirname}/views/index.html`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
